d0521e7b63fdb41b855da35d686b437ba4b1dc7c
Zachary Flight <zachary@grotonbiblechapel.org>
2026-07-09 10:29:05 -0400

    Fixed ghupdate

--- scripts/ghupdate.sh
+++ scripts/ghupdate.sh
@@ -16,7 +16,7 @@ lastcommit=$(git log --pretty=format:"%H")
 
 if [[ $lastcommit != $lastupdate ]]
 then
-    mv /home/pi/csdb/csdbghupdate.sh /home/pi/csdbghupdate.sh
+    mv /home/pi/csdb/scripts/ghupdate.sh /home/pi/csdbghupdate.sh
     ( sleep 60; mv /home/pi/csdbghupdate.sh /home/pi/scripts/csdb/ghupdate.sh ) & 
 
     # Crons
